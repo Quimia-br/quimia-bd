b17f898142a20c6883b208a6ead8504301ef1682
DaviLacerdaHub <davioliveiralac@gmail.com>
2026-09-22 15:54:34 -0300

    feat(routines): cria tabelas de log de auditoria minimos

--- src/database/sql/routines/triggers/trg_auditoria_fds.sql
+++ src/database/sql/routines/triggers/trg_auditoria_fds.sql
@@ -1,3 +1,6 @@
--- DROP TRIGGER IF EXISTS trg_auditoria_fds ON fds;
--- DROP TRIGGER IF EXISTS auditoria_fds ON fds;
+DROP TRIGGER IF EXISTS trg_auditoria_fds ON fds;
 
+CREATE TRIGGER trg_auditoria_fds
+AFTER INSERT OR UPDATE OR DELETE ON fds
+FOR EACH ROW
+EXECUTE FUNCTION fn_auditoria();
