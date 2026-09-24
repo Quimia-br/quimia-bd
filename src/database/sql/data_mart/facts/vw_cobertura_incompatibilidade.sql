6e90191d0cab7aa18a231007948878d2e0287f0f
DaviLacerdaHub <davioliveiralac@gmail.com>
2026-09-24 18:14:27 -0300

    feat(data-mart): refina funcionamento das views, adicionando window functions e CTEs onde possivel

--- src/database/sql/data_mart/facts/vw_cobertura_incompatibilidade.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW vw_cobertura_incompatibilidade AS
-SELECT
-    cq.nome AS classe_quimica,
-    COUNT(DISTINCT ir.id) AS total_regras_envolvendo_classe
-FROM classe_quimica cq
-LEFT JOIN incompatibilidade_regra ir
-    ON ir.id_classe_a = cq.id OR ir.id_classe_b = cq.id
-GROUP BY cq.nome;
